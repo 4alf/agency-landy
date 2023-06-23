7fa31fe329481ca9782fa84c2072e57cc08a3beb
4alf <kingalifa11@gmail.com>
2023-06-24 00:25:45 +0100

    🏗  imports

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import { FacebookLogo, TwitterLogo, InstagramLogo } from 'utils/Icons';
+
+
+const Footer: React.FC = () => {
+
+
+
+    
+}
